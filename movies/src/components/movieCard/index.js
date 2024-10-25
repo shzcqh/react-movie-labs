c014e0c33f16060280952e455b3d8eb29110447f
unknown <2867459421@qq.com>
2024-10-25 21:08:32 +0800

    Movie Details coming from API.

--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -12,7 +12,7 @@ import StarRateIcon from "@mui/icons-material/StarRate";
 import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid2";
 import img from '../../images/film-poster-placeholder.png'
-
+import { Link } from "react-router-dom";
 export default function MovieCard(props) {
   const movie = props.movie;
   return (
@@ -46,9 +46,11 @@ export default function MovieCard(props) {
         <IconButton aria-label="add to favorites" onClick={null}>
           <FavoriteIcon color="primary" fontSize="large" />
         </IconButton>
+        <Link to={`/movies/${movie.id}`}>
         <Button variant="outlined" size="medium" color="primary">
           More Info ...
         </Button>
+        </Link>
       </CardActions>
     </Card>
   );
