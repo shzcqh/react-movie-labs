6cb4d6b8dc4762f0747cae576ccd692c9f6ccdba
unknown <2867459421@qq.com>
2024-10-25 20:39:36 +0800

    Added filtering of movie list - Movie lab 1 complete!

--- movies/src/components/filterMoviesCard/index.js
+++ movies/src/components/filterMoviesCard/index.js
@@ -37,16 +37,17 @@ export default function FilterMoviesCard(props) {
           // eslint-disable-next-line
       }, []);
     
-      const handleChange = (e, type, value) => {
-        e.preventDefault()
-        // Completed later
-      };
+      
       const handleTextChange = e => {
         handleChange(e, "name", e.target.value)
       }
       const handleGenreChange = e => {
         handleChange(e, "genre", e.target.value)
       };
+      const handleChange = (e, type, value) => {
+        e.preventDefault()
+        props.onUserInput(type, value)   // NEW
+      }
   return (
     <Card 
       sx={{
@@ -59,18 +60,23 @@ export default function FilterMoviesCard(props) {
           Filter the movies.
         </Typography>
         <TextField
-          sx={{...formControl}}
-          id="filled-search"
-          label="Search field"
-          type="search"
-          variant="filled"
-        />
+      sx={{...formControl}}
+      id="filled-search"
+      label="Search field"
+      type="search"
+      variant="filled"
+      value={props.titleFilter}
+      onChange={handleTextChange}
+    />
         <FormControl sx={{...formControl}}>
           <InputLabel id="genre-label">Genre</InputLabel>
           <Select
-            labelId="genre-label"
-            id="genre-select"
-          >
+    labelId="genre-label"
+    id="genre-select"
+    defaultValue=""
+    value={props.genreFilter}
+    onChange={handleGenreChange}
+  >
             {genres.map((genre) => {
               return (
                 <MenuItem key={genre.id} value={genre.id}>
